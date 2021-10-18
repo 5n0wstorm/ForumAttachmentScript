b50d02415813724b07a547444520c83e90743ba3
MandoCoding <88238944+MandoCoding@users.noreply.github.com>
2021-10-18 19:11:24 +0100

    Update ForumAttachmentDownloadScript.user.js

--- ForumAttachmentDownloadScript.user.js
+++ ForumAttachmentDownloadScript.user.js
@@ -41,7 +41,7 @@
 
 
 // ==/UserScript==
-// testing as
+// testing words
 const imgurBase = 'https://i.imgur.com/{hash}.mp4';
 /**
 * Set to 'true', if you wanna be asked to input zip name on your own.
